0bfbeee226e90764d714f8ac2706c7b76b0c40a0
skprasan85 <skprasan@gmail.com>
2019-06-25 12:00:47 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectComponent } from './project/project.component';
+import { UserComponent } from './user/user.component';
+import { AddtaskComponent } from './task/addtask/addtask.component';
+import { ViewtaskComponent } from './task/viewtask/viewtask.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectComponent,
+    UserComponent,
+    AddtaskComponent,
+    ViewtaskComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    RouterModule.forRoot([
+      {path: 'projects', component: ProjectComponent},
+      {path: 'users', component: UserComponent},
+      {path: 'addTask', component: AddtaskComponent},
+      { path: 'viewTask', component: ViewtaskComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
